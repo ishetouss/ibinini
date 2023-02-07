483564b520cb9e7b34540cb4083a1d296b7b02e1
ishetouss <ishetou.123@gmail.com>
2023-02-07 18:28:48 +0200

    new push

--- passenger_wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-from project.wsgi import application
-
-sys.path.insert(0, os.path.dirname(__file__))
-
-environ = os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
